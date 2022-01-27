7a722260c62c8fb23d5439e24ad93a442deacdc7
Naseem <1610915@brunel.ac.uk>
2022-01-27 15:07:16 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import productsData from './products.json';
+import { HttpClient } from "@angular/common/http";
+import $ from "jquery";
+
+interface Product {
+  id: Number;
+  category: String;
+  name: String;
+  price: String;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +17,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'epos-system';
+
+  products: Product[] = productsData;
+  // data:any = []
+  // console.log("logging");
+  //
+  // constructor(private http: HttpClient) {
+  //
+  // }
+
+  addRow() {
+    const tbody = document.getElementsByTagName("tbody")[0];
+    let row = document.createElement("tr");
+
+    let cell1 = document.createElement("td");
+    let cellText1 = document.createTextNode("name"); //testing if it displays something onclick
+
+    let cell2 = document.createElement("td");
+    let cellText2 = document.createTextNode("quantity");
+
+    let cell3 = document.createElement("td");
+    let cellText3 = document.createTextNode("price");
+
+    cell1.appendChild(cellText1);
+    cell2.appendChild(cellText2);
+    cell3.appendChild(cellText3);
+
+    row.appendChild(cell1);
+    row.appendChild(cell2);
+    row.appendChild(cell3);
+
+    tbody.appendChild(row);
+  }
+
 }
